46e836e35bd034ffa49b23f4f9e5fe301e8f908e
masudrana5 <abdullainfo30@gmail.com>
2023-09-01 22:07:35 +0600

    book created done

---
+++ src/app/modules/book/book.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
+import { BookController } from './book.controller';
+import { BookValidation } from './book.validation';
+
+const router = Router();
+
+router.post(
+  '/create-book',
+  auth(ENUM_USER_ROLE.ADMIN),
+  validateRequest(BookValidation.create),
+  BookController.insertIntoDB
+);
+
+export const BookRoutes = router;
